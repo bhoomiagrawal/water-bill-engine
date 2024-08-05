86f24b2122a524cd235d4d50190acbcf30098946
pooja <pooja.kumawat2412@gmail.com>
2024-08-05 13:58:21 +0530

    MODIFY : Changes in UI

--- src/components/FirstUI.js
+++ src/components/FirstUI.js
@@ -6,46 +6,46 @@ import { calculateWaterBill } from "@/components/calc";
 
 let readings = [
   {
-      category: "d",
-      connection_size: "15mm",
-      consumption: "45000",
-      meter_status: "mf",
-      severage: "yes",
+    category: "d",
+    connection_size: "15mm",
+    consumption: "45000",
+    meter_status: "mf",
+    severage: "yes",
   },
   {
-      category: "d",
-      connection_size: "15mm",
-      consumption: "20000",
-      meter_status: "mf",
-      severage: "yes",
+    category: "d",
+    connection_size: "15mm",
+    consumption: "20000",
+    meter_status: "mf",
+    severage: "yes",
   },
   {
-      category: "d",
-      connection_size: "15mm",
-      consumption: "15000",
-      meter_status: "mf",
-      severage: "yes",
+    category: "d",
+    connection_size: "15mm",
+    consumption: "15000",
+    meter_status: "mf",
+    severage: "yes",
   },
   {
-      category: "d",
-      connection_size: "15mm",
-      consumption: "10000",
-      meter_status: "mf",
-      severage: "no",
+    category: "d",
+    connection_size: "15mm",
+    consumption: "10000",
+    meter_status: "mf",
+    severage: "no",
   },
   {
-      category: "d",
-      connection_size: "15mm",
-      consumption: "15001",
-      meter_status: "mf",
-      severage: "yes",
+    category: "d",
+    connection_size: "15mm",
+    consumption: "15001",
+    meter_status: "mf",
+    severage: "yes",
   },
   {
-      category: "d",
-      connection_size: "15mm",
-      consumption: "16000",
-      meter_status: "mf",
-      severage: "no",
+    category: "d",
+    connection_size: "15mm",
+    consumption: "16000",
+    meter_status: "mf",
+    severage: "no",
   },
 ];
 let waterBill = calculateWaterBill(readings);
@@ -56,57 +56,94 @@ export default function FirstUI() {
   const [bill, setBill] = useState(null);
 
   const [displayItem, setDisplayItem] = useState(false)
- 
+
 
   const getUI = () =>
     readings?.map((r, i) => {
-        return (
-            <ul key={i} className="flex justify-between">
-                <li> category: {r.category}</li>
-                <li> Connection Size: {r.connection_size}</li>
-                <li> consumption: {r.consumption}</li>
-                <li> meter status: {r.meter_status}</li>
-                <li> water charge: {r.waterCharge?.toFixed(2)}</li>
-                <li> minimum charge: {r.minimum?.toFixed(2)}</li>
-                <li> basic charge: {r.basicCharge?.toFixed(2)}</li>
-                <li> severage charge: {r.severageCharge?.toFixed(2)}</li>
+      return (
 
-                <li> Fixed charge: {r.fixedCharge?.fixed_charge}</li>
-                <li> Meter Service charge: {r.fixedCharge?.service_charge}</li>
-                <li> total Fixed charge: {r.fixedCharge?.total_fixed_charge}</li>
-                <li> bill: {r.bill?.toFixed(2)}</li>
-            </ul>
-        );
+
+
+        <tr key={i} >
+          <td>  {r.category}</td>
+          <td>  {r.connection_size}</td>
+          <td>  {r.consumption}</td>
+          <td>  {r.meter_status}</td>
+          <td>  {r.waterCharge?.toFixed(2)}</td>
+          <td>  {r.minimum?.toFixed(2)}</td>
+          <td>  {r.basicCharge?.toFixed(2)}</td>
+          <td>  {r.severageCharge?.toFixed(2)}</td>
+
+          <td>  {r.fixedCharge?.fixed_charge}</td>
+          <td>  {r.fixedCharge?.service_charge}</td>
+          <td>  {r.fixedCharge?.total_fixed_charge}</td>
+          <td>  {r.bill?.toFixed(2)}</td>
+        </tr>
+
+
+
+      );
     });
   return (
     <>
-      <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
+      <div className="bg-blue-200 relative isolate overflow-hidden py-24 sm:py-32">
         <img
           alt=""
           src="/water-1560478_1280.png"
-          style={{opacity: '0.6'}}
+          style={{ opacity: '0.6' }}
           // src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
-          className=" absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
+          className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
         />
-        {/* <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl -mt-20">Work with us</h2> */}
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-7xl lg:mx-0">
-          <h2 className="text-4xl font-bold tracking-tight text-white -mt-20">Water billing system</h2>
-      <ReadExcel displayItem={displayItem} setDisplayItem={setDisplayItem}/>
+        <div className="m-4">
+          <div className=" max-w-full lg:mx-0">
+            <h2 className="m-2 text-4xl font-bold tracking-tight text-gray -mt-20">Water billing system</h2>
+            <ReadExcel displayItem={displayItem} setDisplayItem={setDisplayItem} />
 
-      <h2>Water Bill Connection</h2>
-      <div className="">{displayItem ? getUI() : ""}</div>
+              {displayItem ? 
+              <>
+               <div className="p-2 m-2">
+              <h2 className="p-2 m-2 mt-8 font-bold">Water Bill Calculation</h2>
+              
+              <table className="table table-striped mt-6 text-lg leading-8 "style={{width:'100%'}}>
+             
+                <thead>
+                  <tr className='row-auto '>
+                    
+                    <th >Category</th>
+                    <th >Connection Size</th>
+                    <th >Consumption</th>
+                    <th >Meter status</th>
+                    <th >Water charge</th>
+                    <th >Minimum charge</th>
+                    <th >Basic charge</th>
+                    <th >Severage charge</th>
+                    <th >Fixed charge</th>
+                    <th >Meter Service charge</th>
+                    <th >Total Fixed charge</th>
+                    <th >Bill</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {  getUI()}
+                </tbody>
+              </table> 
+              </div>
+              </>
+           
+              : ""}
+          </div>
+
         </div>
-       
-      </div>
 
       </div>
       <div>
-      <footer style={{textAlig:'center',padding:'10px',
-        backgroundColor:'#f0f8ff',
-        color:'#007acc'}}>
-        <p>&copy; {new Date().getFullYear()} Water Billing Co.</p>
-      </footer>
+        <footer style={{
+          textAlig: 'center', padding: '10px',
+          backgroundColor: '#f0f8ff',
+          color: '#007acc'
+        }}>
+          <p>&copy; {new Date().getFullYear()} Water Billing Co.</p>
+        </footer>
       </div>
 
 
@@ -114,36 +151,3 @@ export default function FirstUI() {
   );
 }
 
-const styles = {
-  formContainer: {
-    backgroundColor: '#f0f8ff',
-    padding: '20px',
-    borderRadius: '10px',
-    boxShadow: '0px 0px 15px rgba(0, 0, 0, 0.1)',
-    maxWidth: '400px',
-    margin: '0 auto',
-    textAlign: 'center',
-  },
-  inputContainer: {
-    marginBottom: '20px',
-  },
-  input: {
-    padding: '10px',
-    borderRadius: '5px',
-    border: '1px solid #ddd',
-    width: '100%',
-    boxSizing: 'border-box',
-  },
-  button: {
-    padding: '10px 20px',
-    backgroundColor: '#007acc',
-    color: '#fff',
-    border: 'none',
-    borderRadius: '5px',
-    cursor: 'pointer',
-  },
-  result: {
-    marginTop: '20px',
-    fontSize: '18px',
-  },
-};
